366a41556e35dd88333269cd11870672fb059bda
wpontus@github.com <wendtpontus@gmail.com>
2023-08-16 14:33:31 +0200

    ny change

--- Pokemon.ps1
+++ Pokemon.ps1
@@ -1,5 +1,5 @@
 cls
-#Goal: Show me the abilities of a pokemon
+#Goal: Show me the moves of a pokemon
 $Pokemonname = "bulbasaur"
 
 
@@ -8,16 +8,8 @@ $url_base = "https://pokeapi.co"
 $url_endpoint = "/api/v2/pokemon/$Pokemonname/"
 $url = $url_base + $url_endpoint
 
-
-
 #Gather the information from API
 $response = Invoke-RestMethod -uri $url -Method Get -ContentType "application/json" -headers $header -Body $body
 
 #Show the result
-#$response #| ConvertTo-Json -Depth 3
-
-#$response.moves
-
-#Get-Content $response | ConvertFrom-Json
-
- $response.moves |Select-Object -ExpandProperty move | select name
+$response.moves |Select-Object -ExpandProperty move | select name
