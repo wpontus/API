a97f04893a67848b87ee6ca2da679b788836f67a
wpontus@github.com <wendtpontus@gmail.com>
2023-08-16 14:40:06 +0200

    ny change

--- Pokemon.ps1
+++ Pokemon.ps1
@@ -1,15 +1,16 @@
-cls
-#Goal: Show me the moves of a pokemon
-$Pokemonname = "bulbasaur"
+function Get-PokemonMoves {
+    param (
+        $Pokemonname
+    )
+    #Make sure to replace the URL values as it makes sense to match your scenario"
+    $url_base = "https://pokeapi.co"
+    $url_endpoint = "/api/v2/pokemon/$Pokemonname/"
+    $url = $url_base + $url_endpoint
 
+    #Gather the information from API
+    $response = Invoke-RestMethod -uri $url -Method Get -ContentType "application/json" -headers $header -Body $body
 
-#Make sure to replace the URL values as it makes sense to match your scenario"
-$url_base = "https://pokeapi.co"
-$url_endpoint = "/api/v2/pokemon/$Pokemonname/"
-$url = $url_base + $url_endpoint
-
-#Gather the information from API
-$response = Invoke-RestMethod -uri $url -Method Get -ContentType "application/json" -headers $header -Body $body
-
-#Show the result
-$response.moves |Select-Object -ExpandProperty move | select name
+    #Show the result
+    Write-Output $response.moves |Select-Object -ExpandProperty move | select name
+}
+Get-PokemonMoves -Pokemonname charizard
